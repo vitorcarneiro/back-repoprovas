9af57667da125bd84371b0620388f44c3283639f
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-04-23 16:08:16 -0300

    feat: sign-in

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import validateSchemaMiddleware from '../middlewares/validateSchemaMiddleware.js';
 import * as authSchemas from '../schemas/authSchema.js';
-import { signUp } from "../controllers/authController.js";
+import { signUp, signIn } from "../controllers/authController.js";
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', validateSchemaMiddleware(authSchemas.signup), signUp);
+authRouter.post('/sign-up', validateSchemaMiddleware(authSchemas.userJoi), signUp);
+authRouter.post('/sign-in', validateSchemaMiddleware(authSchemas.userJoi), signIn);
 
 export default authRouter;
