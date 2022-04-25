177d811a28eacb2ba88c7cae10ef1ba7257f068e
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-04-25 19:08:19 -0300

    feat: token validation

--- src/middlewares/ensureAthenticationMiddleware.ts
+++ src/middlewares/ensureAthenticationMiddleware.ts
@@ -1,18 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
+import jwt from "jsonwebtoken";
+import * as authService from "../services/authService.js";
 import { unauthorized } from './handleErrorsMiddleware.js';
 
-export function ensureAuthenticationMiddleware(
+export async function ensureAuthenticationMiddleware(
     req: Request,
     res: Response,
     next: NextFunction
 ) {
     const auth = req.headers['authorization'];
     if (!auth) throw unauthorized("Missing authorization header");
-
+    console.log('auth', auth);
+    
     const token = auth.replace("Bearer ", "");
+    console.log('token', token);
     if (!token) throw unauthorized("Missing token");
 
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET) as {
+        userId: number;
+    };
     
-
-
+    const user = await authService.findById(userId);
+    res.locals.user = user;
+    
+    console.log(user);
+    
+    next();
 }
